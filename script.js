d79e3e4be03a90ba6fbdaa8cfd02780a548db9f2
ashley <alandis01@gmail.com>
2022-12-29 14:33:34 -0500

    added timer function, added sections for different questions in HTML as well as query selectors for all in Java

--- script.js
+++ script.js
@@ -7,24 +7,80 @@ var endOfGameScreen = document.querySelector("#endOfGameScreen");
 var score = document.querySelector("#score");
 var initialsInput = document.querySelector("#initialsInput");
 var highScoreScreen = document.querySelector("#highScoreScreen");
-
+var timeLeft = 5
+var timerInput = document.querySelector("#timer");
       
 var timer = function () {
+    var timerInterval = setInterval(() => {
+       timeLeft --;
+       timerInput.textContent ="Time Left " + timeLeft + "s";
+       console.log(timeLeft);
+       if (timeLeft <= 0) {
+        clearInterval(timerInterval)
+       }
+    }, 1000);
 };
 
 var displayQuestion = function () {
 };
 
-var 
-egsgdg
-
 startButton.addEventListener("click", function() {
     timer();
     displayQuestion();
-    var sections = document.querySelectorAll("setion");
+    var sections = document.querySelectorAll("section");
     for (var i = 0; i < sections.length; i++) {
         sections[i].style.display = "none";
     }
     challengeScreen.style.display = "block";
 });
 
+// local storage - initials 
+// timer - subtract from clock when answer incorrectly 
+// make if statement for when timer hits 0 game is over?
+// when answer a question, nmove onto the next question 
+
+
+// function setCounterText() {
+//     countEl.textContent = count;
+    
+//   }
+//   function add(){
+//     count++;
+//     setCounterText()
+  
+//   }
+//   function subtract(){
+//     if(count > 0){
+//     count--;}
+//     setCounterText()
+//   }
+ 
+//   incrementEl.addEventListener("click", add);
+
+// decrementEl.addEventListener("click", subtract);
+
+
+// ___________________________________________________
+
+// var count = localStorage.getItem("count");
+
+// counter.textContent = count;
+
+// addButton.addEventListener("click", function() {
+//   if (count < 24) {
+//     count++;
+//     counter.textContent = count;
+//     localStorage.setItem("count", count);
+//   }
+// });
+
+// subtractButton.addEventListener("click", function() {
+//   if (count > 0) {
+//     count--;
+//     counter.textContent = count;
+//     localStorage.setItem("count", count);
+//   }
+// });
+
+
+// event.prevent default
