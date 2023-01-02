00d7c9aed23f73d8e31624e25341ba5d4ba6fc5d
ashley <alandis01@gmail.com>
2023-01-02 13:42:26 -0500

    deleting code not working starting over focusing more on Java versus HTML original version

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var challengeContainer = document.querySelector("#challengeContainer");
 var beginningScreen = document.querySelector("#beginningScreen");
 var startButton = document.querySelector("#startButton");
-var challengeScreen = document.querySelector("#challengeScreen");
+var section = document.querySelector("section");
+// var question1 = document.querySelector("#question1");
+// var question2 = document.querySelector("#question2");
+// var question3 = document.querySelector("#question3");
+// var question4 = document.querySelector("#question4");
+// var question5 = document.querySelector("#question5");
 var buttonUl = document.querySelector("#buttonUl");
 var endOfGameScreen = document.querySelector("#endOfGameScreen");
 var score = document.querySelector("#score");
 var initialsInput = document.querySelector("#initialsInput");
 var highScoreScreen = document.querySelector("#highScoreScreen");
-var timeLeft = 5
+var timeLeft = 75
 var timerInput = document.querySelector("#timer");
       
 var timer = function () {
@@ -18,7 +23,7 @@ var timer = function () {
        if (timeLeft <= 0) {
         clearInterval(timerInterval)
        }
-    }, 1000);
+    }, 1000);  
 };
 
 var displayQuestion = function () {
@@ -31,48 +36,23 @@ startButton.addEventListener("click", function() {
     for (var i = 0; i < sections.length; i++) {
         sections[i].style.display = "none";
     }
-    challengeScreen.style.display = "block";
+    // section.style.display = "block";
+    question1.style.display = "block";
+    beginningScreen.setAttribute("class", "hide");
 });
 
-// local storage - initials 
-// timer - subtract from clock when answer incorrectly 
-// make if statement for when timer hits 0 game is over?
-// when answer a question, nmove onto the next question 
+function handleChoice(event) {
+    var button = event;
+    console.log("button", button);
+    // console.log(this.getAttribute (value));
+    console.log("button.value", button.value);
+    if (button.value !== "correct") {
+        alert ("incorrect");
+    } else {
+        alert ("correct");
+    }
+}
 
-
-// function setCounterText() {
-//     countEl.textContent = count;
-    
-//   }
-//   function add(){
-//     count++;
-//     setCounterText()
-  
-//   }
-//   function subtract(){
-//     if(count > 0){
-//     count--;}
-//     setCounterText()
-//   }
- 
-//   incrementEl.addEventListener("click", add);
-
-// decrementEl.addEventListener("click", subtract);
-
-
-// ___________________________________________________
-
-// var count = localStorage.getItem("count");
-
-// counter.textContent = count;
-
-// addButton.addEventListener("click", function() {
-//   if (count < 24) {
-//     count++;
-//     counter.textContent = count;
-//     localStorage.setItem("count", count);
-//   }
-// });
 
 // subtractButton.addEventListener("click", function() {
 //   if (count > 0) {
